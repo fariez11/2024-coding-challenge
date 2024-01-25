b515c8e4d81f8c2a8bb790563dbe0fe4defaacae
unknown <fariezilham03@gmail.com>
2024-01-26 00:50:27 +0700

    challenge 4/336

---
+++ day4.js
@@ -0,0 +1,20 @@
+// WPU Coding Challenge 2024
+// Sentence smash
+// 4/336
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+// arrow func
+const smash = (words) => words.join(' ')
+
+// barbar 
+// function smash(words){
+//     let result = ''
+//     for (let i = 0; i < words.length; i++) {
+//         (i != words.length -1) ? result += `${words[i]} ` : result += `${words[i]}`  
+//     }
+//     return result
+// }
+
+// result 
+
+console.log(smash(['hello', 'world', 'this', 'is', 'great']));
