7be869e6211049180d7497ddd2d9cb3dca581993
unknown <fariezilham03@gmail.com>
2024-02-23 10:41:38 +0700

    challenge #18

--- day18.js
+++ day18.js
@@ -4,3 +4,26 @@
 // https://www.codewars.com/kata/5ab6538b379d20ad880000ab
 
 // code
+
+// barbar version
+function greet(name, owner) {
+    if (name === owner) {
+        return 'Halo Boss'
+    } else {
+        return `Halo Dek`
+    }
+
+    // if(name === owner) return `Halo Bos`         \   pak sandhika version
+    // if(name !== owner) return `Halo Dek`         /
+
+}
+
+// using ternary + one liner
+const greet2 = (name, owner) => name === owner ? `Hello boss` : `Hello guest`
+
+// using string template literals
+function greet3(name, owner) {
+    return `Hello ${name == owner ? 'boss' : 'guest'}`
+}
+
+console.log(greet3('Sandhika', 'Sandhika'));
