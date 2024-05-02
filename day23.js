0e4c38a50df3d081b70ec14eec1a1d7d20d8a88c
unknown <fariezilham03@gmail.com>
2024-05-02 10:53:22 +0700

    challenge #23

---
+++ day23.js
@@ -0,0 +1,27 @@
+// WPU Coding Challenge 2024
+// Count the Monkeys!
+// 23/336
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+// code
+
+function monkeyCount(n) {
+    let result = []
+    for (let i = 1; i <= n; i++) {
+        result.push(i)
+    }
+    return result
+}
+
+
+// using Array.from (pak sandhika)
+function monkeyCount2(n){
+    // return Array.from({length : n}, (v,i) => i + 1)
+    return Array.from({length : n}, (_,i) => i + 1)
+}
+
+    // oneliner
+
+    const monkeyCount3 = monkey => Array.from({length : monkey}, (_,i) => i + 1)
+
+console.log(monkeyCount2(10));
