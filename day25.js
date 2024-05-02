6bde35ee5a7327a0554c19dc1f4124232638858b
unknown <fariezilham03@gmail.com>
2024-05-02 14:29:17 +0700

    challenge #25

---
+++ day25.js
@@ -0,0 +1,24 @@
+// WPU Coding Challenge 2024
+// Double Char
+// 25/336
+// https://www.codewars.com/kata/56b1f01c247c01db92000076
+
+// code
+
+function doubleChar(str){
+    let result = ''
+
+    for (let i = 0; i < str.length; i++) {
+        result += str[i] + str[i]
+        
+        // answer from pak sandhika
+            // result += str[i]
+            // result += str[i]
+    }
+
+    return result
+}
+
+const doubleChar2 = str => [...str].map(el => el + el).join('')
+
+console.log(doubleChar2('Adidas'));
