ed2868b98d4a489d7a7ddce2fc082793f12ab23e
unknown <fariezilham03@gmail.com>
2024-05-06 16:35:31 +0700

    challenge #29 & #30

---
+++ day29.js
@@ -0,0 +1,31 @@
+// WPU Coding Challenge 2024
+// Removing Elements
+// 29/336
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2
+
+// bar bar
+
+function removeEveryOther(arr){
+    let result = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 == 0 ) {
+            result.push(arr[i])
+        }        
+    }
+
+    return result
+}
+
+// more answer from pak sandhika
+function removeEveryOther2(arr) {
+    return arr.filter((el,i) => i % 2 == 0);
+    // return arr.filter((el, i) => !(i & 1));
+}
+    // oneliner
+    // const removeEveryOther3 = (arr) => arr.filter((el, i) => i % 2 == 0);
+
+console.log(removeEveryOther2(['Hello', 'Goodbye', 'Hello Again']))
+
+// catatan : 
+    // - bentuk sintax filter = filter((elemen, index, array) => { ... })
---
+++ day30.js
@@ -0,0 +1,19 @@
+// WPU Coding Challenge 2024
+// Will there be enough space?
+// 30/366
+// https://www.codewars.com/kata/5875b200d520904a04000003
+
+// barbar 
+
+function enough(cap, on, wait) {
+    if(cap >= on + wait){
+        return 0
+    }else{ 
+        return wait - (cap - on)
+    }
+}
+
+// oneliner
+const enough1 = (cap, on, wait) => (cap >= on + wait) ? 
+
+console.log(enough(100,60,50));
