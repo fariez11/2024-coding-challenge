7e60f2857d5d79a43622bb7cd23cb99c770824df
fariez <69473438+fariez11@users.noreply.github.com>
2024-09-18 09:12:49 +0700

    update challenge#29

--- day29.js
+++ day29.js
@@ -23,9 +23,22 @@ function removeEveryOther2(arr) {
     // return arr.filter((el, i) => !(i & 1));
 }
     // oneliner
+    removeEveryOther2 = (arr) => arr.filter((el, i) => i % 2 == 0)
+
+function removeEveryOther3(arr){
+    let result = []
+    for(let i = 0; i < arr.length; i+=2){
+        result.push(arr[i])
+    }
+
+    return result
+}
+    // oneliner
     // const removeEveryOther3 = (arr) => arr.filter((el, i) => i % 2 == 0);
 
 console.log(removeEveryOther2(['Hello', 'Goodbye', 'Hello Again']))
+
+console.log(removeEveryOther3([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 // catatan : 
     // - bentuk sintax filter = filter((elemen, index, array) => { ... })
